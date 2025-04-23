3458a7cbe99a2e08648f6a7c79c9ebf3edf711b7
M-Saleh-N <salehnasser65784@gmail.com>
2025-04-23 17:36:22 +0300

    added a passed

--- test_div.py
+++ test_div.py
@@ -3,7 +3,11 @@ from calculator import Calculator
 def test_divi():
     calc = Calculator()
 
-    assert calc.divi(2, 2) == 1
-    assert calc.divi(15, 5) == 3
-    assert calc.divi(12, 1) == 12
+    try:
+
+        assert calc.divi(2, 2) == 1
+        assert calc.divi(15, 5) == 3
+        assert calc.divi(12, 1) == 12
+    except ValueError as e :
+        assert str(e) == "Cannot divide by zero"
 
